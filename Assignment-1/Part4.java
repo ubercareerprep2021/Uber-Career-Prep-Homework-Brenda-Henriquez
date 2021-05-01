78ddcbd8bffe7f16daa524125584e8602abd7038
brendahenriquez <bhenriqu@masonlive.gmu.edu>
2021-05-01 04:07:38 -0400

    linked list skeleton

--- Assignment-1/Part4.java
+++ Assignment-1/Part4.java
@@ -1,1 +1,58 @@
 
+class Part4{
+
+    Node head;
+    Node rear; 
+    int size; 
+
+    class Node
+    { 
+        int data;
+        Node next;
+
+        //constructor
+        Node (int d){
+            data = d;
+            next = null;
+        }
+    }
+
+    void push (Node node){
+        head = node;
+        size++;
+    }
+
+    Node pop(){
+        
+        return null;
+    }
+
+    
+    void insert(int index, Node node){
+
+    }
+
+    void remove(int index){
+
+    }
+
+    Node elementAt(int index){
+        return null;
+    }
+
+    int size(){
+        return -1;
+    }
+
+    void printList(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+}
+
+
+
