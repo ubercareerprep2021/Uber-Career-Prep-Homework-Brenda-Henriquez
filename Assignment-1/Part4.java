c75d56767388d918ecc5e481ac662403f6d1d3ac
brendahenriquez <bhenriqu@masonlive.gmu.edu>
2021-05-13 18:36:35 -0400

    part 5

--- Assignment-1/Part4.java
+++ Assignment-1/Part4.java
@@ -1,9 +1,4 @@
-
 class Part4{
-
-    Node head;
-    Node rear; 
-    int size; 
 
     class Node
     { 
@@ -17,43 +12,111 @@ class Part4{
         }
     }
 
-    part4(){
-        head = null;
-        rear = null;
-        size = 0;
-    }
+    Node head = null;
+    Node tail = null;
+    int size = 0;
 
+    //add node to the end of the list
     void push (Node node){
-        head = node;
+        if(head == null){ //if list is empty
+            head = node;
+            tail = node;
+        }
+        else{
+            //old tail will point to the newly added node
+            tail.next = node;
+            //the new node that was added will become the new tail
+            tail = node;
+        }
         size++;
     }
 
-    Node pop(){
+    //remove last node and return data
+    Integer pop(){
+
         if(head == null) return null;
-        int res = rear.data;
+        if(head.next == null) return null;
 
-        return null;
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        int result = temp.data;
+        temp.next = null;
+        size--;
+        return result;
     }
 
-    
+    ////
     void insert(int index, Node node){
+        if((index > size) || (index < 0)) return;
 
+        Node temp = head;
+        Node prev = null;
+        int count = 0;
+        while(count != index){
+            count++;
+            temp = node;
+            temp = temp.next;
+            if(index == 1){
+                
+            }
+
+        }
+        head = head.next;
     }
-
+/////--------------
     void remove(int index){
+        if((index < 0) || (index > size)) return;
+        if(head ==null) return;
 
+        Node temp = head;
+ 
+
+        if (index == 0){
+            head = temp.next; 
+            return;
+        }
+
+        for (int i=0; temp!=null && i<index-1; i++)
+            temp = temp.next;
+
+        if (temp == null || temp.next == null)
+            return;
+
+        Node next = temp.next.next;
+ 
+        temp.next = next;  // Unlink the deleted node from list
+  
     }
 
     Node elementAt(int index){
-        return null;
+        if((index < 0) || (index > size)) return null;
+
+        int count = 0;
+        Node temp = head;
+        while(count <= index){
+            temp = temp.next;
+            count++;
+        }
+        return temp;
     }
 
     int size(){
-        return -1;
+        return size;
     }
 
     void printList(){
-
+        Node temp = head;
+        String res = "";
+        while(temp != null){
+            res += temp.data;
+            temp = temp.next;
+            if(temp != null){
+                res += ",";
+            }
+        }
+        System.out.print(res);
     }
 
     public static void main(String[] args){
--- Assignment-1/Part5.java
+++ Assignment-1/Part5.java
@@ -1,1 +1,103 @@
+import java.util.*;
+
+import org.graalvm.compiler.core.common.util.ReversedList;
+
+class Part5{
+
+    class Node
+    { 
+        int data;
+        Node next;
+
+        //constructor
+        Node (int d){
+            data = d;
+            next = null;
+        }
+    }
+
+    static Node head = null;
+    static Node tail = null;
+    static int size = 0;
+
+
+    //Iterativley
+    public static Node reverseList(Node head) {
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+            System.out.println("oooo");
+        }
+        return prev;
+    }
+
+    //Using a stack
+    public static Node reverseListStack (Node head) {
+        Stack<Integer> st = new Stack<Integer>();
+
+        Node curr = head;
+        while (curr != null) {
+            st.push(curr.data);
+            curr = curr.next;
+        }
+
+        Node temp = head;
+        while(!(st.empty())){
+            temp.data = st.pop();
+            temp = temp.next;
+        }
+
+        return head;
+    }
+
+    //Recursivley
+    public static Node reverseListRec (Node head) {
+        if (head == null || head.next == null) return head;
+        Node p = reverseListRec(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+
+
+    public static void main(String[] args){
+ 
+        // Part4 ls = new Part4();
+        // head.data = 0;
+        // ls.head.data = 1;
+        // ls.head.next.data = 2;
+        // ls.head.next.next.data =3;
+        // ls.head.next.next.next.data =4;
+
+        LinkedList<Integer> ls = new LinkedList<Integer>();
+        ls.add(1);
+        ls.add(2);
+        ls.add(3);
+        System.out.println(reverseList(head));
+
+
+
+        // uberQueue ub = new uberQueue();
+        // ub.enqueue(1);
+        // ub.enqueue(2);
+        // ub.enqueue(3);
+        // System.out.println(ub.size());
+        // System.out.println(ub.front());
+        // System.out.println(ub.rear());
+        // System.out.println(ub.dequeue());
+
+    }
+
+
+
+}
+
+
+
+
+
 
